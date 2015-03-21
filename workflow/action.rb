bc9d456769875b10a068ac1f07d02852cb4e4e7c
Micah Rosales <micah.rosales@gmail.com>
2015-03-20 17:42:02 -0700

    Updated workflow to use rbenv properly instead of system ruby

--- workflow/action.rb
+++ workflow/action.rb
@@ -1,25 +1,35 @@
 #!/usr/bin/env ruby
+
+# action.rb
+# createdby: Micah Rosales
+# modified:  12/27/14
+#
+# Called by an alfred workflow with a JSON blob
+# as the first argument. Parameter must have a
+# type field and a arg field
+
 require "json"
 
-def logFile (filename, log)
-    File.open(filename, 'w+') { |file|
-        file.puts(log)
-    }
-end
-
+# Copies the given string to the OSX system
+# clipboard using pbcopy and piping to stdin
 def pbcopy(input)
     str = input.to_s
     IO.popen('pbcopy', 'w') { |f| f << str }
     str
 end
 
-args = JSON.parse(ARGV[0], :symbolize_names => true)
-arg = args[:arg]
-type = args[:type]
+begin
+    args = JSON.parse(valid, :symbolize_names => true)
+    arg = args[:arg]
+    type = args[:type]
+rescue NameError
+    puts "Could not parse JSON!"
+end
 
-
+# geo cmds will have lat,lon format arg
 if type == 'geo'
     `open "http://maps.apple.com/?sll=#{arg}"`
 else
+    # Otherwise copy to sys clipboard
     pbcopy(arg)
 end
